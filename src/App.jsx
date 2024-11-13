5ed1a3164e1aaa3d4afcaac59773b902f556197f
ViraRiefertDev <vira.riefert.dev@gmail.com>
2024-11-13 20:07:37 +0100

    consult02

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import "./App.css";
-import Lesson03 from "./lessons/Lesson03/Lesson03";
-
-
 //lesson imports
 /* import Lesson02 from "./lessons/Lesson02/Lesson02"; */
+/* import Lesson03 from "./lessons/Lesson03/Lesson03"; */
 
 //homework imports
 /* import Homework02 from "./homeworks/Homework02/Homework02"; */
 
 //consultation imports
+import Consult02 from "./consultations/Consult02/Consult02";
 
 function App() {
   return (
     <div className="App">
       {/* <Lesson02 /> */}
       {/* <Homework02 /> */}
-      <Lesson03 />
+      {/* <Lesson03 /> */}
+      <Consult02 />
     </div>
   );
 }
