5b4bc7c39de64252c8e4611c357eb6b3c969458b
ViraRiefertDev <vira.riefert.dev@gmail.com>
2024-11-14 18:29:09 +0100

    some changes

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -6,9 +6,9 @@ function LoginForm(){
   return(
     <form className="login-form-wrapper" method='POST'>
       <h4 className="login-form-title">Login form</h4>
-      <Input name={'email'} type={'email'} placeholder={'Enter your email'} label={'Email'}/>
-      <Input name={'password'} type={'password'} placeholder={'Enter your password'} label={'Password'}/>
-      <Button name={'Login'} type={'submit'}/>
+      <Input name='email' type='email' placeholder='Enter your email' label='Email'/>
+      <Input name='password' type='password' placeholder='Enter your password' label='Password'/>
+      <Button name='Login' type='submit'/>
     </form>
   )
 }
